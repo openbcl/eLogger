8eb385bc9f2193b229037ecf386cbbb6df40c4d8
Robert Weiße <rweisse@users.noreply.github.com>
2021-11-29 14:02:10 +0100

    Init Angular App

---
+++ build.js
@@ -0,0 +1,27 @@
+// This file should be called automatically by "npm run <...>"-command (adjusts environment files based on hosts environment or given args)
+
+const fs = require('fs');
+const path = require('path');
+const packageJson = require('./package.json');
+
+fs.readFile('.git/HEAD', (err, head) => {
+    const filepath = path.join(__dirname, 'src', 'environments', 'build.json');
+    const versionData = {
+        version: packageJson.version,
+        branch: '',
+        commit: ''
+    }
+    if (!err) {
+        const rev = head.toString();
+        if (rev.indexOf(':') > -1) {
+            fs.readFile('.git/' + rev.substring(5).replace(/\n/g, ''), (err, hash) => {
+                if (!err) {
+                    versionData.branch = rev.substring(5).replace(/\n/g, '').split('/').pop();
+                    versionData.commit = hash.toString().substr(0, 7);
+                }
+                fs.writeFile(filepath, JSON.stringify(versionData), 'utf8', (err) => err && console.log(err));
+            });
+        }
+    }
+    fs.writeFile(filepath, JSON.stringify(versionData), 'utf8', (err) => err && console.log(err));
+});
