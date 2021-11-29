a5129db70c3b421b63cf4bfb7c2ea661e448f279
Robert Weiße <rweisse@users.noreply.github.com>
2021-11-29 14:25:17 +0100

    Framework + PWA Updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'el-root',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'elogger';
+  title = 'eLogger';
+
+  constructor(
+    private swUpdate: SwUpdate
+  ){ }
+
+  ngOnInit(): void {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.versionUpdates.pipe(
+        filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY')
+      ).subscribe(() => window.location.reload());
+    }
+  }
+
 }
