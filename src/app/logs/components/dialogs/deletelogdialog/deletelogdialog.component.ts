c1fad5a1bf44197d42b56ac938338d4d86051d52
rweisse <github@robert-weisse.de>
2022-02-14 14:42:27 +0100

    Try delete catch OSI layer 8

--- src/app/logs/components/dialogs/deletelogdialog/deletelogdialog.component.ts
+++ src/app/logs/components/dialogs/deletelogdialog/deletelogdialog.component.ts
@@ -1,5 +1,7 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { Store } from '@ngrx/store';
+import { AppValidators } from '../../../../shared/utils/validators';
 import { Log } from '../../../../shared/models';
 import { deleteLog } from '../../../store/log.actions';
 
@@ -8,7 +10,7 @@ import { deleteLog } from '../../../store/log.actions';
   templateUrl: './deletelogdialog.component.html',
   styleUrls: ['./deletelogdialog.component.scss']
 })
-export class DeleteLogDialogComponent {
+export class DeleteLogDialogComponent implements OnChanges {
 
   @Input()
   log: Log;
@@ -16,11 +18,24 @@ export class DeleteLogDialogComponent {
   @Output()
   logChange = new EventEmitter<Log>();
 
-  constructor(private store: Store) { }
+  compare = { value: '' };
+  form = this.fb.group({ title: [null, AppValidators.isEqualString(this.compare)] });
 
+  constructor(
+    private store: Store,
+    private fb: FormBuilder
+  ) { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (!!changes['log']?.['currentValue']?.title?.length) {
+      this.compare.value = this.log.title;
+    }
+  }
+  
   close() {
     this.log = undefined;
     this.logChange.emit(this.log);
+    this.form.reset();
   }
 
   delete() {
--- src/app/shared/utils/validators.ts
+++ src/app/shared/utils/validators.ts
@@ -1,9 +1,10 @@
-import { AbstractControl, AsyncValidatorFn, ValidationErrors } from "@angular/forms";
+import { AbstractControl, AsyncValidatorFn, ValidationErrors, ValidatorFn } from "@angular/forms";
 import { map, Observable, take } from "rxjs";
 import { LogTemplate } from "../models";
 import { compareEventTemplates } from "./helper";
 
 export const eventTypeIsUniqueError = 'eventTypeIsUnique';
+export const isEqualStringError = 'isEqualString';
 
 export class AppValidators {
     static eventTypeIsUnique = (logTemplate$: Observable<LogTemplate>): AsyncValidatorFn => {
@@ -13,5 +14,9 @@ export class AppValidators {
                 compareEventTemplates(eventTemplate, control.value)) ? null : { [eventTypeIsUniqueError]: true }
             )
         )
+    }
+
+    static isEqualString = (input: { value: string }): ValidatorFn => {
+        return(control: AbstractControl): ValidationErrors | null => input.value === control.value ? null : { [isEqualStringError]: true }
     }
 }
