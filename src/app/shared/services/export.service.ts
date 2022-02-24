d424b9357a1306f92ea9ab56356c8bab7f79ed40
rweisse <github@robert-weisse.de>
2022-02-24 14:04:01 +0100

    Records row order changed + CSV export charset

--- src/app/shared/services/export.service.ts
+++ src/app/shared/services/export.service.ts
@@ -20,26 +20,24 @@ export class ExportService {
             const csv: string[][] = [];
             const containStart = !!records.find(r => r.eventType === EventType.START);
             const containData = !!records.find(r =>  [EventType.TEXT, EventType.AUDIO, EventType.PICTURE].includes(r.eventType));
-            let headers = [ 'name', 'type', 'absolute time' ];
-            if (containStart) {
-                headers.push('relative time');
-            }
+            const headers = [ 'Name', 'Type' ];
             if (containData) {
-                headers.push('data');
+                headers.push('Data');
+            }
+            headers.push('Absolute Time');
+            if (containStart) {
+                headers.push('Relative Time');
             }
             csv.push(headers);
             records.forEach(record => {
                 const relTime = this.eventRelTime.transform(record, records);
-                const row = [
-                    record.name,
-                    this.eventLabel.transform(record.eventType),
-                    this.date.transform(record.date, 'yyyy-MM-dd, HH:mm:ss')
-                ];
-                if (containStart) {
-                    row.push(!!relTime.time ? `${relTime.prefix}${this.date.transform(relTime.time, 'HH:mm:ss.SSS', 'UTC+0')}`: relTime.prefix)
-                }
+                const row = [ record.name, this.eventLabel.transform(record.eventType) ];
                 if (containData) {
                     row.push(!!record.text?.length ? record.text : '');
+                }
+                row.push(this.date.transform(record.date, 'yyyy-MM-dd, HH:mm:ss'));
+                if (containStart) {
+                    row.push(!!relTime.time ? `${relTime.prefix}${this.date.transform(relTime.time, 'HH:mm:ss.SSS', 'UTC+0')}`: relTime.prefix)
                 }
                 csv.push(row);
             });
@@ -49,7 +47,7 @@ export class ExportService {
     }
 
     private provideFileDownload(data: string, type: string, filename: string = 'test.csv') {
-        const blob = new Blob([data], { type });
+        const blob = new Blob(['\ufeff', data], { type });
         const element = window.document.createElement('a');
         element.href = window.URL.createObjectURL(blob);
         element.download = filename;        
