ce42383f98187a852fa4f8270cf3b815c7e2e555
rweisse <github@robert-weisse.de>
2022-03-29 11:22:29 +0200

    Maximized camera dialogs if is pwa

--- src/app/components/basedialog/basedialog.component.ts
+++ src/app/components/basedialog/basedialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Dialog } from "primeng/dialog";
 
 @Component({ template: '' })
 export abstract class BaseDialogComponent {
@@ -11,5 +12,15 @@ export abstract class BaseDialogComponent {
     close() {
         this.visible = false;
         this.visibleChange.emit(this.visible);
+    }
+
+    showDialogMaximized(dialog: Dialog, checkPWA = false) {
+        if (checkPWA) {
+            if (['fullscreen', 'standalone', 'minimal-ui'].some((displayMode) => window.matchMedia('(display-mode: ' + displayMode + ')').matches)) {
+                dialog.maximize()
+            }
+        } else {
+            dialog.maximize();
+        }
     }
 }
