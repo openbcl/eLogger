90ee7728bbf3e1e52c800dcea9f46321b4605318
rweisse <github@robert-weisse.de>
2022-04-12 18:37:07 +0200

    Show current diff time while recording

--- src/app/logs/components/record/record.component.ts
+++ src/app/logs/components/record/record.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 import { combineLatest, filter, map, switchMap, interval, of } from 'rxjs';
 import { PrimeIcons } from 'primeng/api';
-import { EventTemplate, EventType } from '../../../models';
+import { EventTemplate, EventType, Record } from '../../../models';
 import { createRecord, loadRecords } from '../../../store/record.actions';
 import { templatesSelector } from '../../../store/template.selectors';
 import { loadLog } from '../../store/log.actions';
@@ -33,17 +33,37 @@ export class RecordComponent implements OnInit {
     this.store.pipe(select(templatesSelector), filter(templates => !!templates)),
     this.store.pipe(select(logIdSelector), filter(logId => !!logId))
   ]).pipe(filter(logData => logData?.[0]?.id === logData?.[2] && !!logData?.[1]?.length));
+  interval$ = interval(this.refreshRate);
   log$ = this.logData.pipe(map(logData => logData[0]));
   template$ = this.logData.pipe(map(logData => logData[1].find(template => template.id === logData[0].templateId)));
   records$ = this.log$.pipe(switchMap(log => this.store.select(recordsSelector(log.id))));
-  absTime$ = interval(this.refreshRate).pipe(map(() => new Date()));
+  absTime$ = this.interval$.pipe(map(() => new Date()));
   relTime$ = this.records$.pipe(switchMap(records => {
       if(!records.find(r => r.eventType === EventType.START) || records.find(r => r.eventType === EventType.END || !!records.length && records[records.length - 1].eventType === EventType.PAUSE)) {
         return of(this.currentEventRelTime.transform(records));
       }
-      return interval(this.refreshRate).pipe(map(() => this.currentEventRelTime.transform(records)));
+      return this.interval$.pipe(map(() => this.currentEventRelTime.transform(records)));
   }));
   relDays$ = this.relTime$.pipe(filter(relTime => !!relTime), switchMap(relTime => of(Math.trunc(+relTime / 86400000))));
+  timeDiff$ = this.records$.pipe(switchMap(records => {
+      if(!records.length || [EventType.PAUSE, EventType.END].includes(records[records.length -1].eventType)) {
+        return of({ days: 0, time: new Date(0) });
+      }
+      let preRecord: Record;
+      let pausedMilliseconds = 0;
+      if (records[records.length -1].eventType === EventType.RESUME) {
+        const reversedRecords = [ ...records ].reverse();
+        preRecord = reversedRecords.find(record => ![EventType.PAUSE, EventType.RESUME].includes(record.eventType));
+        const preRecordIndex = reversedRecords.findIndex(record => record.key === preRecord.key);
+        for (let i = 0; i < preRecordIndex; i = i + 2) {
+          pausedMilliseconds -= +reversedRecords[i].date - +reversedRecords[i + 1].date;
+        }
+      }
+      return this.interval$.pipe(map(() => {
+        const time = records[records.length -1].eventType !== EventType.RESUME ? new Date(+new Date() - +records[records.length -1].date) : new Date(+new Date() - +preRecord.date + pausedMilliseconds);
+        return { days: Math.trunc(+time / 86400000), time };
+      }));
+  }));
 
   constructor(
     private store: Store,
